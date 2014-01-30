15807ab271dcada6e2cb3ca2adde9311bbc3a124
Pierre <lauret.p@gmail.com>
2014-01-30 10:15:43 +0100

    Initialisation de l'appli

---
+++ app/cache/dev/twig/30/c8/91bbba227033e91a56ba7634128156839abd9d8531d4137a58810826c50f.php
@@ -0,0 +1,39 @@
+<?php
+
+/* DC3InstawallBundle:Default:index.html.twig */
+class __TwigTemplate_30c891bbba227033e91a56ba7634128156839abd9d8531d4137a58810826c50f extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello ";
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : $this->getContext($context, "name")), "html", null, true);
+        echo "!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "DC3InstawallBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
---
+++ src/DC3/InstawallBundle/DC3InstawallBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DC3\InstawallBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DC3InstawallBundle extends Bundle
+{
+}
